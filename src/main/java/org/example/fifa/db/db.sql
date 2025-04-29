22356103b2887f9e79c859c91fbfce3cba1af3e4
onja-phi <hei.onja.2@gmail.com>
2025-04-29 12:54:40 +0300

    feat: adjust database syntaxes

--- src/main/java/org/example/fifa/db/db.sql
+++ src/main/java/org/example/fifa/db/db.sql
@@ -1,84 +1,115 @@
-\c postgres
-   drop database if exists championnat_europeenne;
-
+\ c postgres drop database if exists championnat_europeenne;
 CREATE DATABASE championnat_europeenne;
-\c championnat_europeenne
-
--- Enum types
-create type position as enum ('GOALKEEPER', 'MIDFIELDER', 'FORWARD', 'DEFENDER');
-
+\ c championnat_europeenne -- Enum types
+create type position as enum (
+    'GOAL_KEEPER',
+    'MIDFIELDER',
+    'STRIKER',
+    'DEFENSE'
+);
+create type status as enum ('NOT_STARTED', 'STARTED', 'FINISHED');
+create type duration_unit as enum ('SECOND', 'MINUTE', 'HOUR');
+-- Season table
+create table season (
+    id varchar primary key,
+    year integer not null,
+    alias varchar(10) not null,
+    status status default 'NOT_STARTED'
+);
 -- Player table
 create table player (
-                        id serial primary key,
-                        name varchar(100),
-                        number integer unique,
-                        age integer,
-                        position position,
-                        nationality varchar(100)
+    id varchar primary key,
+    name varchar(100) not null,
+    number integer unique,
+    age integer,
+    position position,
+    nationality varchar(100),
+    club_id varchar references club(id)
 );
-
 -- Coach table
 create table coach (
-                       id serial primary key,
-                       name varchar(100) unique,
-                       nationality varchar(100)
+    id varchar primary key,
+    name varchar(100) unique,
+    nationality varchar(100)
 );
-
 -- Stadium table
 create table stadium (
-                         id serial primary key,
-                         name varchar(100)
+    id serial primary key,
+    name varchar(100)
 );
-
 create table league (
-                        id serial primary key,
-                        name varchar(100)
+    id serial primary key,
+    name varchar(100)
 );
-
 create table championship (
-                              id serial primary key,
-                              period_id integer references period(id)
-)
-
--- Club table
+    id serial primary key,
+    period_id integer references period(id)
+) -- Club table
 create table club (
-                      id serial primary key,
-                      name varchar(100),
-                      acronym varchar(10),
-                      creation_date timestamp,
-                      stadium_id integer references stadium(id),
-                      coach_id integer references coach(id),
-                      league_id integer references league(id)  -- club maromaro anatina league iray
+    id varchar primary key,
+    name varchar(100) not null,
+    acronym varchar(3),
+    year_creation integer,
+    stadium varchar(100),
+    coach_id varchar references coach(id)
 );
-
 -- Match table
-create table matches (
-                       id serial primary key,
-                       date timestamp,
-                       stadium_id integer references stadium(id), -- manana stade iray
-                       championship_id references championship(id)  ,-- match maromaro anaty championnat iray
-                        home_club_id integer references club(id),
-                        away_club_id integer references club(id),
-                        check (home_club_id <> away_club_id) -- les 2 clubs sont different
+create table "match" (
+    id varchar primary key,
+    club_playing_home varchar references club(id),
+    club_playing_away varchar references club(id),
+    stadium varchar(100),
+    match_datetime timestamp,
+    status status default 'NOT_STARTED',
+    season_id varchar references season(id)
 );
-
-
-create table score (
-                       id serial primary key,
-                       rang integer,
-                       matches_id integer references matches(id),
-                       club_id integer references club(id),
-                       points integer
+-- Goal table
+create table goal (
+    id serial primary key,
+    match_id varchar references "match"(id),
+    club_id varchar references club(id),
+    player_id varchar references player(id),
+    minute_of_goal integer check (
+        minute_of_goal between 1 and 90
+    ),
+    own_goal boolean default false
 );
-
+-- Club Statistics table
+create table club_statistics (
+    id serial primary key,
+    club_id varchar references club(id),
+    season_id varchar references season(id),
+    ranking_points integer default 0,
+    scored_goals integer default 0,
+    conceded_goals integer default 0,
+    difference_goals integer default 0,
+    clean_sheet_number integer default 0,
+    unique(club_id, season_id)
+);
+-- Player Statistics table
+create table player_statistics (
+    id serial primary key,
+    player_id varchar references player(id),
+    season_id varchar references season(id),
+    scored_goals integer default 0,
+    playing_time_value numeric,
+    playing_time_unit duration_unit,
+    unique(player_id, season_id)
+);
+create table match_score (
+    id serial primary key,
+    rang integer,
+    matches_id integer references matches(id),
+    club_id integer references club(id),
+    points integer
+);
 create table period (
-                        id serial primary key,
-                        date_format_string varchar(20);
+    id serial primary key,
+    date_format_string varchar(20);
 );
-
 create table ranking (
-                         id serial primary key,
-                         number integer unique,
-                         club_id integer references club(id),
-                         points integer
+    id serial primary key,
+    number integer unique,
+    club_id integer references club(id),
+    points integer
 );
